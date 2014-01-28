a38ec7d6b92b107b678de93f5b725606497f4e9d
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-01-28 14:19:10 -0500

    Test that the field-based analyzer is applied

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -14,6 +14,7 @@ from django.test.utils import override_settings
 from haystack.fields import CharField as HaystackCharField
 
 from elasticstack import backends
+from elasticstack import fields
 
 
 class TestBackendSettings(TestCase):
@@ -64,5 +65,18 @@ class TestSchema(TestCase):
                 index_fieldname='body')
         # build_schema is passed a SortedDict of search index fields keyed by
         # field name
+        schema = back_class.build_schema({'body': text_field})
+        self.assertEqual("stop", schema[1]['body']['analyzer'])
+
+    def test_field_analyzer(self):
+        """Ensure that field analyzer works on a case by case basis"""
+        back_class = backends.ConfigurableElasticBackend('default', URL="",
+                INDEX_NAME="")
+        # Control test - by default the CharField does not have a keyword
+        # argument named 'analyzer' and does not take **kwargs
+        self.assertRaises(TypeError, HaystackCharField, document=True,
+                          use_template=True, index_fieldname='body', analyzer='stop')
+        text_field = fields.CharField(document=True, use_template=True,
+                index_fieldname='body', analyzer='stop')
         schema = back_class.build_schema({'body': text_field})
         self.assertEqual("stop", schema[1]['body']['analyzer'])

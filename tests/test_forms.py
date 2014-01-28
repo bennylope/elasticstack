b5b3b3360db7db7f8cc406d33aa11cc68932623f
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-01-28 14:35:44 -0500

    Test basic form functionality

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -8,12 +8,22 @@ test_elasticstack
 Tests for `elasticstack` forms module.
 """
 
+from django import forms
 from django.test import TestCase
 
-from elasticstack import forms
+from elasticstack.forms import SearchForm
 
 
-class TestElasticstack(TestCase):
+class TestForms(TestCase):
 
-    def setUp(self):
-        pass
+    def test_named_search_field(self):
+        """Ensure that the `q` field can be optionally used"""
+
+        class MyForm(SearchForm):
+            s = forms.CharField(label='Search')
+            f = forms.CharField(label='More search')
+            search_field_name = 's'
+
+        form = MyForm()
+        self.assertTrue('s' in form.fields)
+        self.assertFalse('q' in form.fields)

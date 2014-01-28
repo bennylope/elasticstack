7d8ec0f79ff2d0423a1d4e5a11546a4cee52c227
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-01-28 15:05:31 -0500

    Minor view testing
    Most of the functionality here is directly provided by Django's mixins.

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,6 +9,7 @@ Tests for `elasticstack` views module.
 """
 
 from django.test import TestCase
+from django.test.client import RequestFactory
 
 from elasticstack import views
 
@@ -16,4 +17,16 @@ from elasticstack import views
 class TestElasticstack(TestCase):
 
     def setUp(self):
-        pass
+        self.factory = RequestFactory()
+
+    def test_form_kwargs(self):
+        """Ensure request data updates the form kwargs"""
+        request = self.factory.request()
+        request.GET = {'q': "whoami"}
+
+        mixin = views.SearchMixin()
+        mixin.request = request
+        mixin.queryset = []  # An EmptySearchQuerySet is basically an empty list
+
+        self.assertEqual(mixin.get_form_kwargs(),
+                {'initial': {}, 'data': request.GET, 'searchqueryset': []})

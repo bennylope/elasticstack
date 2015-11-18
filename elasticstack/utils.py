a787416a10b0ca66e405166255de006b9e890165
Mario César <mariocesar.c50@gmail.com>
2015-11-18 11:51:09 -0400

    Add compatible get_model method
    get_model is deprecated in Django 1.9. Taken from django-oscar loading utils

--- elasticstack/utils.py
+++ elasticstack/utils.py
@@ -22,6 +22,8 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from haystack import connections
+from importlib import import_module
+from django.apps import apps
 
 
 def prepare_object(obj, using='default'):
@@ -38,3 +40,35 @@ def prepare_object(obj, using='default'):
     for key, value in prepped_data.items():
         final_data[key] = connections[using].get_backend()._from_python(value)
     return final_data
+
+
+def get_model(app_label, model_name):
+    """
+    Fetches a Django model using the app registry.
+    This doesn't require that an app with the given app label exists,
+    which makes it safe to call when the registry is being populated.
+    All other methods to access models might raise an exception about the
+    registry not being ready yet.
+    Raises LookupError if model isn't found.
+    """
+    try:
+        return apps.get_model(app_label, model_name)
+    except AppRegistryNotReady:
+        if apps.apps_ready and not apps.models_ready:
+            # If this function is called while `apps.populate()` is
+            # loading models, ensure that the module that defines the
+            # target model has been imported and try looking the model up
+            # in the app registry. This effectively emulates
+            # `from path.to.app.models import Model` where we use
+            # `Model = get_model('app', 'Model')` instead.
+            app_config = apps.get_app_config(app_label)
+            # `app_config.import_models()` cannot be used here because it
+            # would interfere with `apps.populate()`.
+            import_module('%s.%s' % (app_config.name, MODELS_MODULE_NAME))
+            # In order to account for case-insensitivity of model_name,
+            # look up the model through a private API of the app registry.
+            return apps.get_registered_model(app_label, model_name)
+        else:
+            # This must be a different case (e.g. the model really doesn't
+            # exist). We just re-raise the exception.
+            raise
